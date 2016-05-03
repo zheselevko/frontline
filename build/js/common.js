4b93f8a8bf71807e61f6c3e91f6e748a1c291828
Evgeniy Selevko <evgeniyselevko@gmail.com>
2016-05-03 23:42:27 +0300

    добавлены новые блоки и фото

--- build/js/common.js
+++ build/js/common.js
@@ -3,3 +3,21 @@ $(document).ready(function() {
 	// go js
 
 });
+function setEqualHeight(columns)
+{
+var tallestcolumn = 0;
+columns.each(
+function()
+{
+currentHeight = $(this).height();
+if(currentHeight > tallestcolumn)
+{
+tallestcolumn = currentHeight;
+}
+}
+);
+columns.height(tallestcolumn);
+}
+$(document).ready(function() {
+setEqualHeight($(".news .l4"));
+});
